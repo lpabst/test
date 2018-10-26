0e7560732d38d7050a9bee92d04e53823de4feee
Loren Pabst <lpabst@Loren-Pabst.local>
2018-10-25 20:04:59 -0600

    removed things i dont want

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,10 @@
 import * as React from "react";
-import AppNavbar from "./component/AppNavbar";
 
 class App extends React.Component<any, any> {
   render() {
     return (
       <div>
-        <AppNavbar />
+        router and stuff
       </div>
     );
   }
--- client/network/fb_config.ts
+++
@@ -1,2 +0,0 @@
-export const AppId = "842830269247201";
-export const ApiVersion = "v1.1";
